fd7fa710c28144495d27a65dc2cf7ebf3cead931
Michal Banas <banas.michal@icloud.com>
2021-01-23 08:11:50 +0100

    Autoprefixer added

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,34 @@ var gulp = require('gulp');
 
 const { src, dest, watch }=require("gulp");
 const compileSass = require("gulp-sass");
+const autoprefixer = require('gulp-autoprefixer');
 
+// ... variables
+var autoprefixerOptions = {
+   
+  };
+
+  var input = "./SCSS/**/*.scss";
+  var output = "./dist/";
+  var sassOptions = {
+    errLogToConsole: true,
+    outputStyle: 'expanded'
+  };
 
 compileSass.compiler = require("node-sass");
 
 const bundleSass = () => {
     return src("./SCSS/**/*.scss")
     .pipe(compileSass().on("eror", compileSass.logError))
+    .pipe(autoprefixer(autoprefixerOptions))
     .pipe(dest("./dist/"));
-
 };
+
 
 const devWatch = () => {
     watch("./SCSS/**/*.scss", bundleSass);
 };
+
 
 exports.bundleSass = bundleSass; 
 exports.devWatch = devWatch;
