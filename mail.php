6e255ab63917f3ced3d4e90fb140d435d4c78a4b
Michal Banas <banas.michal@icloud.com>
2021-02-09 21:05:24 +0100

    performance update

--- mail.php
+++ mail.php
@@ -13,7 +13,5 @@ if(isset($_POST['submit'])){
     mail($to,$subject,$správa,$headers);
     mail($from,$subject2,$správa2,$headers2); // sends a copy of the message to the sender
     header("Location:thankyou.html");
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
-    // You cannot use header and echo together. It's one or the other.
     }
 ?>
