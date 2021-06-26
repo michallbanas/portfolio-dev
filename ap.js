30be3c87b3ee545cb9d90285a5b6523d6c57f9b5
michallbanas <banas.michal@icloud.com>
2021-06-26 19:47:59 +0200

    safari 12 support, function improving

--- ap.js
+++ ap.js
@@ -1,13 +1,25 @@
-// Theme-Slider
+// Theme-Slider + Safari 12 Toolbar Color Change
 
-const btn = document.querySelector(".theme-slider");
+let btnColorSwitcher = document.querySelector(".theme-slider")
 
-btn.addEventListener("change", function () {
-  document.documentElement.classList.toggle("dark-theme");
-});
+function colorChange () {
+  let headerColorDark = "#101010"
+  let headerColorLight = "#262223"
+  let metaColor = document.querySelector('meta[name="theme-color"]')
+  let changeTheme = document.documentElement.classList.toggle("dark-theme")
+
+  if (changeTheme)
+      metaColor.setAttribute("content", headerColorDark)
+    else metaColor.setAttribute("content", headerColorLight)
+}
+
+btnColorSwitcher.addEventListener("change", colorChange)
 
 // Scrolling-animation
 
 window.addEventListener('scroll', () => {
-  document.body.style.setProperty('--scroll',window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
-}, false);
+  document.body.style.setProperty('--scroll',window.pageYOffset / (document.body.offsetHeight - window.innerHeight))
+}, false)
+
+
+
