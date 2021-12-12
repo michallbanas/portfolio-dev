346a382b12a68cc2571dc945b8242271a70f78e4
michallbanas <banas.michal@icloud.com>
2021-12-12 19:56:40 +0100

    update

--- cypress/integration/titlesTest.js
+++ cypress/integration/titlesTest.js
@@ -5,9 +5,9 @@
 describe('Test', () => {
     it('should find h1, h2, check if they are visible and not empty and have css font raleway', () => {
         cy.visit('/')
-        const nadpisy = ['h1', 'h2']
-        nadpisy.forEach(nadpis => {
-            cy.get(nadpis)
+        const titles = ['h1', 'h2']
+        titles.forEach(title => {
+            cy.get(title)
                 .should('be.visible') // assert that element is visible
                 .and('not.be.empty') // assert that element has content
 
