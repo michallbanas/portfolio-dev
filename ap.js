ffc7d33b554dc8d0512afd0401ac9f5d6803a047
michallbanas <banas.michal@icloud.com>
2022-03-20 16:37:02 +0100

    add: dark mode

--- ap.js
+++ ap.js
@@ -1,9 +1,46 @@
-// Dark Mode at the Operating System Level
+'use strict';
 
-const darkMode = window.matchMedia("(prefers-color-scheme: dark)");
+/* Dark Mode at the Operating System Level */
 
-if (darkMode.matches){
-  document.body.classList.add("dark-theme");
-} else {
-  document.body.classList.remove("dark-theme");
+class DarkMode {
+  constructor() {
+    this.darkMode = window.matchMedia("(prefers-color-scheme: dark)");
+    }
+    set darkMode(darkMode) {
+      darkMode.matches ? document.body.classList.add("dark-theme") : document.body.classList.remove("dark-theme");
+    }
 }
+
+/* The New Safari Tab Layout (Safari 15+) */
+
+class SafariTabLayout {
+  constructor() {
+    this.safari = navigator.vendor;
+  }
+
+    set safari(safari) {
+          const headerDark = '#181818';
+          const headerLight = '#FFF6E8'
+          const metaColor = document.querySelector('meta[name="theme-color"]');
+          const body = document.querySelector('body');
+
+          safari === "Apple Computer, Inc." && body.classList.contains('dark-theme') ? 
+          metaColor.setAttribute('content', headerDark) : metaColor.setAttribute('content', headerLight);
+    }
+}
+
+  /* Google Chrome Auto Dark Mode off (experimental) */
+
+class ChromeAutoDarkOff {
+  constructor() {
+      this.chrome = navigator.vendor;
+    }
+    set chrome(chrome) {
+      const meta = document.querySelector('meta[name="color-scheme"]');
+      chrome === "Google Inc." ? meta.setAttribute('content', 'only light') : null
+    }
+}
+
+const darkMode = new DarkMode();
+const safariTabLayout = new SafariTabLayout();
+const chromeAutoDarkOff = new ChromeAutoDarkOff();
