a426bc88631ad6639626afe145fcbf77bfa8a2b7
michalbanas <michal.banas@kiwi.com>
2022-03-25 18:00:09 +0100

    change: command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,15 +1,6 @@
 /// <reference types="cypress" />
 
 Cypress.Commands.add('req', () => {
-    cy.request({
-        method: 'GET',
-        url: '/'
-    })
-    .then((response) => {
-        const [status, server, date] = [response.status, response.headers.server, response.headers.date];
-        cy.log(`**Response status: ${status}**`);
-        cy.log(`Server: ${server}`);
-        cy.log(`Date: ${date}`);
-    })
+    cy.log('Hello friend, you are testing michalbanas.dev with Cypress, enjoy it ♡')
 })
 
