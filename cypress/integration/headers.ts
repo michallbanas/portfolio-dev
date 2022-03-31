4bf88e8fe5f1b36f4d1c2fa0b3d254571fea1ba5
michalbanas <michal.banas@kiwi.com>
2022-03-31 17:24:51 +0200

    extend: CY test for headers

--- cypress/integration/headers.ts
+++ cypress/integration/headers.ts
@@ -6,6 +6,17 @@ describe('Test', () => {
         cy.visit('/');
     });
 
+    it('should find h1 and check it contains correct text', () => {
+        cy.get('h1').as('title')
+        cy.get('@title').should('be.visible')
+                        .and('contain', 'michal banaš');
+        cy.get('@title').should('have.attr', 'data-cy', 'title');
+    })
+
+    it('should find subheading and check it have correct text', () => {
+        cy.get('[data-cy="heading"]').should('have.text', 'QA Engineer');
+    })
+
     it('should find h1, h2, check if they are visible, not empty and have font family Raleway', () => {
         cy.get('h1, h2').as('headers')
         cy.get('@headers').each(($header) => {
