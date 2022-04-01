ae31f65fa9f633ff0baf837224a183124cc87f0e
michalbanas <michal.banas@kiwi.com>
2022-04-01 16:03:25 +0200

    update: prettier adjustments

--- cypress/integration/anchors.ts
+++ cypress/integration/anchors.ts
@@ -1,21 +1,21 @@
 /// <reference types="cypress" />
 
 describe('Test', () => {
-    beforeEach(() => {
-        cy.req()
-        cy.visit('/');
-    });
-    
-    it('should find all anchor attributes and check if they have href attribute', () => {
-        cy.get('a').each(($link) => {
-            expect($link).to.have.attr('href');
-        })       
-    })
+  beforeEach(() => {
+    cy.req();
+    cy.visit('/');
+  });
 
-    it('should find buy me a coffee anchor and test if', () => {
-        cy.get('.mail-c')
-            .should('contain', 'Buy me a coffee')
-            .invoke('attr', 'target')
-            .should('eq', '_blank')
-    })
-})
+  it('should find all anchor attributes and check if they have href attribute', () => {
+    cy.get('a').each(($link) => {
+      expect($link).to.have.attr('href');
+    });
+  });
+
+  it('should find buy me a coffee anchor and test if', () => {
+    cy.get('.mail-c')
+      .should('contain', 'Buy me a coffee')
+      .invoke('attr', 'target')
+      .should('eq', '_blank');
+  });
+});
