1e4e9e01d15423822651b862c15fe3390535bbde
michalbanas <michal.banas@kiwi.com>
2022-04-01 16:02:26 +0200

    extend: pw test for headers

--- playwright/header.spec.js
+++ playwright/header.spec.js
@@ -6,12 +6,20 @@ test.beforeEach(async ({ page }) => {
     await page.goto('');
   });
 
-test.describe('h1', async () => {
-  test('check if the h1 is correct', async ({ page }) => {
+test.describe('headings', async () => {
+  test('find h1 and check if it is visible and it has correct title', async ({ page }) => {
     expect(page.url()).toBe('http://localhost:3000/')
     const title = page.locator('h1');
     await expect(title).toBeVisible()
     await expect(title).toContainText('michal banaš')
+    await expect(title).toHaveAttribute('data-cy', 'title')
     await page.pause()
   });
+
+  test('find first h2 and check if it has correct title', async ({ page }) => {
+    const heading = page.locator('[data-cy="heading"]')
+    await expect(heading).toBeVisible()
+    await expect(heading).toHaveText('QA Engineer')
+    await page.pause()
+  })
 })
