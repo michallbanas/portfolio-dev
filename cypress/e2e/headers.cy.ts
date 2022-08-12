c5a15be044d8091e607b081a703aa8238ed130b5
michalbanas <michal.banas@kiwi.com>
2022-08-12 18:10:33 +0200

    improve: headers test

--- cypress/e2e/headers.cy.ts
+++ cypress/e2e/headers.cy.ts
@@ -5,22 +5,35 @@ describe('Test', () => {
     cy.visit('/')
   })
 
-  it('should find h1 and check it contains correct text', () => {
-    cy.get('h1').as('title')
-    cy.get('@title').should('contain', 'michal', 'banaš')
-    cy.get('@title').should('have.attr', 'data-cy', 'title')
+  it('should find title in </head> element and verify the text', () => {
+    cy.title().should('eq', 'Michal Banaš - QA Engineer')
   })
 
-  it('should find subheading and check it have correct text', () => {
-    cy.get('[data-cy="heading"]').should('have.text', 'QA Engineer')
+  it('should find h1 and check it has correct text', () => {
+    cy.get('h1')
+      .should('be.visible')
+      .and('have.length', 1)
+      .and('have.text', '| michal banaš')
+      .and('have.attr', 'id', 'title')
   })
 
-  it('should find h1, h2, check if they are visible, not empty and have font family Raleway', () => {
-    cy.get('h1, h2').as('headers')
-    cy.get('@headers').each($header => {
-      expect($header).to.be.visible
-      expect($header).to.not.be.empty
-      expect($header).to.have.css('font-family', 'Raleway, sans-serif')
-    })
+  it('should find subheading and check it has correct text', () => {
+    cy.get('[data-cy="header"]')
+      .should('be.visible')
+      .and('have.text', 'QA Engineer')
+      .and('have.attr', 'class', 'header')
+  })
+
+  it('should find h1, h2, check if they are have font family Raleway', () => {
+    cy.get('h1, h2')
+      .should('have.length', 5)
+      .each($header => {
+        expect($header).to.have.css('font-family', 'Raleway, sans-serif')
+      })
+      .filter('.h-blog, .h-fotografia, .h-contact')
+      .each($filteredElements => {
+        const filteredHeaders = ['Blog', 'Fotografia', 'O mne']
+        expect($filteredElements.text()).be.oneOf(filteredHeaders)
+      })
   })
 })
