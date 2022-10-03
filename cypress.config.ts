4ce75896d019cff4f67cb2a38b12f4cb26c338c1
michalbanas <michal.banas@kiwi.com>
2022-10-03 16:39:03 +0200

    feat: add experimental support for Webkit

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,16 +1,15 @@
-import { defineConfig } from "cypress"
+import { defineConfig } from 'cypress'
 
 export default defineConfig({
   defaultCommandTimeout: 6000,
   experimentalFetchPolyfill: true,
-  retries: 1,
   video: false,
   viewportHeight: 1050,
   viewportWidth: 1680,
   projectId: 'otico9',
   e2e: {
     baseUrl: 'http://localhost:3000',
+    experimentalWebKitSupport: true,
+    supportFile: 'cypress/support/index.ts',
   },
 })
-
-
