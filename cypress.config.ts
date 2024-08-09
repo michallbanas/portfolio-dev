fe543e12e9af1bfc0ed7809d4cd75c739c0fe71c
Michal Banas <banas.michal@icloud.com>
2024-08-09 22:13:31 +0200

    chore: simplify everything part one (#59)

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,16 +2,12 @@ import { defineConfig } from "cypress"
 
 export default defineConfig({
   defaultCommandTimeout: 6000,
-  experimentalFetchPolyfill: true,
   retries: { runMode: 2, openMode: 1 },
-  screenshotOnRunFailure: false,
   video: false,
   viewportHeight: 1050,
   viewportWidth: 1680,
-  watchForFileChanges: false,
   e2e: {
     baseUrl: "http://localhost:3100",
-    experimentalWebKitSupport: true,
     supportFile: "cypress/support/index.ts",
   },
 })
